9640a079d2df86340fd3f2f9ec524f1832579437
Jin Choi <jin.choi@cern.ch>
2021-12-02 21:26:59 +0900

    Freeze 211202

--- Practice-10/EX10-3.C
+++ Practice-10/EX10-3.C
@@ -15,21 +15,16 @@ int main() {
 	srand(time(NULL));
 	
 	const unsigned long max_iter = pow(10, 4);
-	const unsigned long N = pow(2, 5);
+	const unsigned long N = pow(2, 8);
 	const unsigned long nbins = 100;
-	FILE *file;
 	long hist[nbins] = {0};
 
 	// make gauss distribution
-	double gaus[max_iter];
+	double gaus[max_iter] = {0};
 	for (unsigned long i = 0; i < max_iter; i++) {
 		double temp[N] = {0};
-		for (unsigned long j = 0; j < N; j++)
-			temp[j] = uniform_generator(-0.5, 0.5);
-		double result = 0.;
-		for (unsigned long j = 0; j < N; j++)
-			result += temp[(i*N+j)%N]; 
-		gaus[i] = result;
+		for (unsigned long j = 0; j < N; j++) temp[j] = uniform_generator(-0.5, 0.5);
+		for (unsigned long j = 0; j < N; j++) gaus[i] += temp[(i*N+j)%N]; 
 	}
 
 	// to make histogram binning (avoid underflow & overflow)
@@ -41,24 +36,21 @@ int main() {
 
 	// scale gaussian outputs and fill the histogram
 	// e.g. if the output is 1.3, than it goes to (1.3 - min)/(max-min) * total-bin-numbers
-	for (unsigned long i = 0; i < max_iter; i++)
-		hist[int((gaus[i]-min)/(max-min)*nbins)]++;
+	for (unsigned long i = 0; i < max_iter; i++) hist[int((gaus[i]-min)/(max-min)*nbins)]++;
 
 	// now calculate standard deviation
 	double mean = 0.;
-	for (unsigned long i = 0; i < max_iter; i++)
-		mean += gaus[i]/max_iter;
+	for (unsigned long i = 0; i < max_iter; i++) mean += gaus[i]/max_iter;
 	double var = 0.;
-	for (unsigned long i = 0; i < max_iter; i++)
-		var += pow(gaus[i] - mean, 2)/max_iter;
+	for (unsigned long i = 0; i < max_iter; i++) var += pow(gaus[i] - mean, 2)/max_iter;
 	double std = sqrt(var);
 	
 	// write the results
+	FILE *file;
 	file = fopen("EX10-3.DAT", "w");
 	fprintf(file, "max:\t%f\tmin:\t%f\n", max, min);
 	fprintf(file, "std:\t%f\n", std);
-	for (unsigned int i = 0; i < nbins; i++)
-		fprintf(file, "%d\t%d\n", i, hist[i]);
+	for (unsigned int i = 0; i < nbins; i++) fprintf(file, "%d\t%d\n", i, hist[i]);
 	fclose(file);
 		
 	return 0;
